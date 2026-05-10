6daaca68607d9fb1bea3ab2269a484fcf3d173f8
Bex Kuriakose <b_kuriakose@mail.harpercollege.edu>
2026-05-10 13:27:44 -0500

    Array based queue implemented. Finshed lab

--- src/cutie.hpp
+++ src/cutie.hpp
@@ -1,16 +1,16 @@
 #include <string>
 
 class Cutie {
-  private:
+private:
     std::string description;
     int cuteness_rating;
 
-  public:
+public:
     Cutie(std::string description, int cuteness_rating) {
-      this->description = description;
-      this->cuteness_rating = cuteness_rating;
+        this->description = description;
+        this->cuteness_rating = cuteness_rating;
     }
 
     virtual std::string get_description() = 0;
     virtual int get_cuteness_rating() = 0;
-}
+};
--- src/hello.cpp
+++ src/hello.cpp
@@ -1,5 +1,40 @@
-#include <string>
 
-std::string hello() {
-   return "Hello World!";
+#include <iostream>
+#include "hello.hpp"
+
+
+using namespace std;
+
+
+QueueTees::QueueTees() {
+    front = 0;
+    back = 0;
+    count = 0;
+    for (int i = 0; i < 10; i++) {
+        cuties[i] = nullptr;
+    }
 }
+void QueueTees::enqueue(Cutie* cutie) {
+    if (count == 10) {
+        cout << "The queue is full." << endl;
+        return;
+    }
+    cuties[back] = cutie;
+    back = (back + 1) % 10;
+    count = count + 1;
+}
+Cutie* QueueTees::dequeue() {
+    if (count == 0) {
+        return nullptr;
+    }
+    Cutie* removed = cuties[front];
+    cuties[front] = nullptr;
+    front = (front + 1) % 10;
+    count = count - 1;
+    return removed;
+}
+int QueueTees::size() const {
+    return count;
+}
+
+
